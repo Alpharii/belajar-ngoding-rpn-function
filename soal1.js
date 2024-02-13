e8d2e3acaf0fa493f15ed0968a1a0d76c7b2250c
alpharii <alphari404@gmail.com>
2024-02-13 20:35:48 +0700

    first commit

---
+++ soal1.js
@@ -0,0 +1,14 @@
+// Tugas 1
+// Buatlah sebuah fungsi bernama shoutOut(), yang mengembalikan nilai berupa "Halo Function!", 
+// yang kemudian akan ditampilkan di console.
+
+//bikin fungsinya disini
+
+function shoutOut(){
+    let halo = "Halo function";
+    return halo
+}
+
+// ------------------------------------
+console.log(shoutOut());
+// ------------------------------------
